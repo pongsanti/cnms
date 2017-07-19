0b00480f4e8334ae6b3dbe9e9f4cd01c37bbe5d2
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-07-20 01:55:46 +0700

    redux reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,32 @@
-import ReactDOM from 'react-dom'
-import HelloWorld from './HelloWorld'
-// css
-import './css/bootstrap/bootstrap.less'
-// import 'bootstrap/dist/css/bootstrap-theme.css'
-import './css/style.scss'
+/* global document */
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import todoApp from './reducers/reducer';
+import { addTodo } from './actions/todo';
 
-var element = document.createElement('div')
-element.id = 'root'
-document.body.appendChild(element)
+import HelloWorld from './HelloWorld';
+
+
+// css
+import './css/bootstrap/bootstrap.less';
+// import 'bootstrap/dist/css/bootstrap-theme.css'
+import './css/style.scss';
+
+
+const element = document.createElement('div');
+element.id = 'root';
+document.body.appendChild(element);
 
 ReactDOM.render(
   HelloWorld(),
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
+
+const store = createStore(todoApp);
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addTodo('Learn about actions'));
+
+unsubscribe();
